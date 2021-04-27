6397ea395f8f7aad3f8d5853f772c2fa92ffca1f
Huiying Shen <hshen@ski.org>
2021-04-26 22:51:33 -0700

    TK + multiprocess work with pozyx setup

---
+++ hello.py
@@ -0,0 +1,13 @@
+from multiprocessing import Process
+
+def fun(name):
+    print(f'hello {name}')
+
+def main():
+
+    p = Process(target=fun, args=('Peter',))
+    p.start()
+
+
+if __name__ == '__main__':
+    main()
